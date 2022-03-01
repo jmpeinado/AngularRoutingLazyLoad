e23abaac78c2843cab0c9bde8fdac9281f5e2cd6
Javier Muñoz Peinado <jmpeinado@indra.es>
2022-03-01 09:28:00 +0100

    implementado modulos con carga lazyload a traves del routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ErrorPageComponent } from './shared/error-page/error-page.component';
+import { AppRoutinngModule } from './app-routinng.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ErrorPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutinngModule
   ],
   providers: [],
   bootstrap: [AppComponent]
